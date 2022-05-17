04ccf1cc254c1c0b8570718a2b82f81f96e1642e
Minino99 <loizzo.pasqualino@gmail.com>
2022-05-17 16:27:44 +0200

    aggiunto pulsante show password

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-let Nome = prompt("Qual'è il tuo nome?");
-let Cognome = prompt("Qual'è il tuo cognome?");
-let ColorePreferito  = prompt("Qual'è il tuo colore preferito?");
-const Anno = "22"
+// let Nome = prompt("Qual'è il tuo nome?");
+// let Cognome = prompt("Qual'è il tuo cognome?");
+// let ColorePreferito  = prompt("Qual'è il tuo colore preferito?");
+let Nome = "ciao"
+let Cognome = "sodaod0'as"
+let ColorePreferito = "kpaodksopa"
+const Anno = "22";
 const generatedPassword = document.getElementById("pssw-gnrtd");
 generatedPassword.innerHTML = Nome + Cognome + ColorePreferito + Anno;
