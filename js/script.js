97a829bd9d9afefd367b0b99fa60eafae3680f42
Minino99 <loizzo.pasqualino@gmail.com>
2022-05-17 16:29:25 +0200

    minor fixes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-// let Nome = prompt("Qual'è il tuo nome?");
-// let Cognome = prompt("Qual'è il tuo cognome?");
-// let ColorePreferito  = prompt("Qual'è il tuo colore preferito?");
-let Nome = "ciao"
-let Cognome = "sodaod0'as"
-let ColorePreferito = "kpaodksopa"
+let Nome = prompt("Qual'è il tuo nome?");
+let Cognome = prompt("Qual'è il tuo cognome?");
+let ColorePreferito  = prompt("Qual'è il tuo colore preferito?");
 const Anno = "22";
 const generatedPassword = document.getElementById("pssw-gnrtd");
 generatedPassword.innerHTML = Nome + Cognome + ColorePreferito + Anno;
